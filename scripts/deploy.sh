f71ff9f86f1f634d0f45a3d30d185e6923a59387
Lucas Pinheiro <lucas.contato1996@gmail.com>
2017-04-14 20:17:10 -0300

    deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+launch-chrome () {
+	echo "Launching google chrome: https://artemis-285b4.firebaseapp.com/"
+	google-chrome "https://artemis-285b4.firebaseapp.com/"
+}
+
+rm -r ./dist/ &&
+npm run build:prod &&
+./node_modules/.bin/firebase deploy &&
+launch-chrome
