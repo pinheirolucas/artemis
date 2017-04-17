fd3115be804e4f900baba4c2b13c1cd24c0c8c50
Lucas Pinheiro <lucas.contato1996@gmail.com>
2017-04-16 22:09:29 -0300

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ launch-chrome () {
 	google-chrome "https://artemis-285b4.firebaseapp.com/"
 }
 
-rm -r ./dist/ &&
+rm -r ./dist/
 npm run build:prod &&
 ./node_modules/.bin/firebase deploy &&
 launch-chrome
