ba97449e400b6322ab9544bf26bf361093c21165
Lucas Pinheiro <lucas.pinheiro@controller.education>
2017-04-17 10:31:58 -0300

    env update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
 		"authorizeUrl":
 			"https://dribbble.com/oauth/authorize?client_id=7d6e74ae6328cf1b6770a3fccf4e0d123936771500d0dc3a972a7094eaee9dc8",
 		"client": "7d6e74ae6328cf1b6770a3fccf4e0d123936771500d0dc3a972a7094eaee9dc8",
-		"secret": "b49971006f20d41e19d814fe3c46bef31bc66c26066ceff2c5df6d879ca0e8ac",
-		"token": "4e1872325ba6e3890697ac08469cbd60cda6abf2b088a98f4ad90dce114965b9"
+		"secret": "141aaca963aae4a2c981078eadaf55c51f680e65e6933ea1aac9371a4a211c34",
+		"token": "397e7f7361065d188d2a7b7e242b7d5b421756f56d7a1060e1c18a65615f2605"
 	}
 };
